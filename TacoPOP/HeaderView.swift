010c8c3bcac9cbec8643c71356791803b1870fbe
Ben Makusha <btmakusha@gmail.com>
2016-09-07 00:35:53 -0400

    Added shadow layout for the UIView header

---
+++ TacoPOP/HeaderView.swift
@@ -0,0 +1,11 @@
+//
+//  HeaderView.swift
+//  TacoPOP
+//
+//  Created by Baynham Makusha on 9/7/16.
+//  Copyright © 2016 Ben Makusha. All rights reserved.
+//
+
+import UIKit
+
+class HeaderView: UIView , DropShadow { }
---
+++ TacoPOP/MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  MainVC.swift
+//  TacoPOP
+//
+//  Created by Baynham Makusha on 9/7/16.
+//  Copyright © 2016 Ben Makusha. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+    
+    @IBOutlet weak var headerView: HeaderView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        headerView.addDropShadow()
+    }
+
+}
